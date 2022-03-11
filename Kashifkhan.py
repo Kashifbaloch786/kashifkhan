7896804a01b3f49ca2796444492de596e51a06bb
Kashifbaloch786 <101421074+Kashifbaloch786@users.noreply.github.com>
2022-03-11 22:54:46 +0500

    Create Kashifkhan.py
    https://github.com/Kashifbaloch786/kashifkhan.git
    ls
    Cd Kashifkhan
    ls 
    Python2 Kashifkhan.py

---
+++ Kashifkhan.py
@@ -0,0 +1,49 @@
+<h1 align="center">Hi 👋, I'm Kashif khan</h1>
+<h3 align="center">Kashif is a passionate Software engineer who loves to learn new things and one thing that he loves more is sharing what he has learned. He is well versed in Modern Android Development concepts with a firm grasp of software architecture and design. Kashif loves to write code that is clean, scalable, and maintainable. He also is a technical writer and writes on medium, with contributions to opensource as well, and has given talks on various technical topics on different events. Kashif has previously worked with well-known names of Pakistan's software industry such as VisionX and Telenor microfinance bank, currently building a product that is backed by Y-Combinator called Markaz (YC-22) </h3>
+
+
+<p align="center">&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=kashif-e&show_icons=true&locale=en" alt="kashif-e" /></p>
+<p align="center"><img align="center" src="https://github-readme-stats.vercel.app/api/top-langs?username=kashif-e&show_icons=true&locale=en&layout=compact" alt="kashif-e" /></p>
+
+
+
+<p align="center"> <img src="https://komarev.com/ghpvc/?username=kashif-e&label=Profile%20views&color=0e75b6&style=flat" alt="kashif-e" /> </p>
+
+<p align="center"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=kashif-e" alt="kashif-e" /></a> </p>
+
+<p align="left"> <a href="https://twitter.com/kashif_mehmood_" target="blank"><img src="https://img.shields.io/twitter/follow/kashif_mehmood_?logo=twitter&style=for-the-badge" alt="kashif_mehmood_" /></a> </p>
+
+- 🔭 I’m currently working on [MARKAZ](https://play.google.com/store/apps/details?id=com.markaz.app)
+
+- 🌱 I’m currently learning **Jetpack Compose**
+
+- 👨‍💻 All of my projects are available at [https://github.com/Kashif-E?tab=repositories](https://github.com/Kashif-E?tab=repositories)
+
+- 📝 I regularly write articles on [https://kashif-mehmood-km.medium.com/](https://kashif-mehmood-km.medium.com/)
+
+- 💬 Ask me about **Android , ARCore , Sceneform , Jetpack , MVVM**
+
+- 📫 How to reach me **gulkashif330@gmail.com**
+
+- ⚡ Fun fact **I cant stop talking when it comes to emerging technologies**
+
+### Blogs posts
+<!-- BLOG-POST-LIST:START -->
+- [Parallel API calls using Coroutines, having different return types](https://towardsdev.com/parallel-api-calls-using-coroutines-having-different-return-types-4d269a1b88d4?source=rss-c403e2b09f16------2)
+- [Parallel API calls with Coroutines, having different return types](https://proandroiddev.com/parallel-api-calls-with-different-return-types-24943c71cbbb?source=rss-c403e2b09f16------2)
+- [Building An OCR app with Entity Extracting Using ML-KIT](https://towardsdev.com/advanced-entity-extraction-and-ocr-with-mlkit-on-android-2b3b768c094a?source=rss-c403e2b09f16------2)
+- [Clean ViewModels with Kotlin Sealed Classes. A Better way to handle Errors](https://kashif-mehmood-km.medium.com/clean-viewmodels-with-kotlin-sealed-classes-a-better-way-to-handle-errors-ae3467081fda?source=rss-c403e2b09f16------2)
+<!-- BLOG-POST-LIST:END -->
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://twitter.com/kashif_mehmood_" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="kashif_mehmood_" height="30" width="40" /></a>
+<a href="https://linkedin.com/in/kashif-mehmood" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="kashif-mehmood" height="30" width="40" /></a>
+<a href="https://stackoverflow.com/users/kashif-mehmood" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/stack-overflow.svg" alt="kashif-mehmood" height="30" width="40" /></a>
+<a href="https://fb.com/kashi.m.456" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="kashi.m.456" height="30" width="40" /></a>
+<a href="https://instagram.com/kashu._98" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="kashu._98" height="30" width="40" /></a>
+<a href="https://medium.com/@kashif-mehmood-km" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/medium.svg" alt="@kashif-mehmood-km" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://developer.android.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg" alt="android"
